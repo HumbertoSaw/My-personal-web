87a52a6dc7e954226c4e4fc630cbf4c18b0f32ae
Humberto S <humbertosc321@gmial.com>
2021-09-01 11:59:10 -0500

    Implementacion de mas botones para navegar en la pagina

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ botonIrArriba.addEventListener("click", function(){
 
 	})		
 });
+
+const botonIrProyectos = document.querySelector("#botonIrProyectos");
+
+botonIrProyectos.addEventListener("click", function(){
+	console.log('test');
+	window.scrollTo({
+		top:600,
+		left:0,
+		behavior: "smooth"
+
+	})		
+});
+
+const botonIrContactos = document.querySelector("#botonIrContactos");
+
+botonIrContactos.addEventListener("click", function(){
+	console.log('test');
+	window.scrollTo({
+		top:1250,
+		left:0,
+		behavior: "smooth"
+
+	})		
+});
